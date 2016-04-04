b9b89dcdffb332e8a3ef72bf6d55d1041ba45f97
Joanne <joanne@caltech.edu>
2016-04-03 18:18:06 -0700

    HW0 completed.

---
+++ HW0/Test.hs
@@ -0,0 +1,4 @@
+f :: Int -> Int
+f x = 3 * x * x + 4 * x + 5
+g :: Double -> Double -> Double
+g x y = x * x + y * y
---
+++ HW0/lab0.hs
@@ -0,0 +1,39 @@
+-- Question 1
+{-
+4.2.0-16-generic
+no response
+no response
+-}
+
+-- Question 2
+{-
+4.2.0-34-generic
+no response
+no response
+
+The version number for the operating system has changed to a more
+recent version.
+-}
+
+-- Question 3
+{-
+4.2.0-34-generic
+/usr/bin/gcc
+no response
+
+GCC has been installed onto the system.
+-}
+
+-- Question 4
+{-
+/opt/ghc/7.10.3/bin/ghc
+/opt/cabal/1.24/bin/cabal
+
+A version of the Haskell compiler has been installed.
+-}
+
+-- Question 5
+{-
+1980
+12.999999999999998
+-}
